ae728622f9e206b8417dc260a7f7904018c3839f
Juan Vargas <juandeveloper19@gmail.com>
2024-11-24 02:05:18 -0500

    Created Post model and add to Django admin panel.
    - Created the Post model with fields for title, content, category, author, created_at, and last_updated.
    - Added the Post model to the Django admin panel for management.
    - Generated and applied database migrations to create the Post table.
    
    The Post model allows users to create and manage blog posts. The model includes fields for title, content, category (linked to Category model), author (linked to User model), and timestamps for creation and last update. The database migrations ensure that the Post table is created in the database when running the migrate command.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -13,3 +14,17 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+
+
+class Post((models.Model)):
+    name = models.CharField(max_length=50, validators=[
+                            MinLengthValidator(5)])
+    content = models.TextField(max_length=2500, validators=[
+        MinLengthValidator(500)])
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + '- by ' + self.user.username
