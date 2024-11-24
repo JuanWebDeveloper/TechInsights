e13187bae7ced5c4cf4c7ab9b5e837ff63330eb4
Juan Vargas <juandeveloper19@gmail.com>
2024-11-24 01:17:47 -0500

    Created Category model and add to Django admin panel.
    - Created the Category model with fields for name and description.
    - Added the Category model to the Django admin panel for management.
    - Included the Meta class in the Category model to customize the plural name displayed in the admin panel.
    - Generated and applied database migrations to create the Category table.
    
    The Meta class with the verbose_name_plural attribute ensures that the plural name for the Category model is displayed as "Categories" instead of the default "Categorys" in the Django admin interface. The database migrations ensure that the Category table is created in the database when running the migrate command.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, validators=[
+                            MinLengthValidator(5)])
+    description = models.TextField(max_length=250, validators=[
+                                   MinLengthValidator(50)])
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
