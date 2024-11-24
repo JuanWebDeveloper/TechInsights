6d791d8cf01eb819c6f8d44a9475787c5c528d22
Juan Vargas <juandeveloper19@gmail.com>
2024-11-24 02:56:52 -0500

    Renamed 'name' field to 'title' in Post model and apply migrations.
    - Renamed the 'name' field to 'title' in the Post model.
    - Defined a default value for the 'title' field to handle existing rows.
    - Updated the database schema to reflect this change.
    - Generated and applied database migrations to ensure the changes are implemented.
    
    This refactor improves the clarity of the Post model by using a more descriptive field name. The database migrations ensure that the changes are correctly applied to the database schema.

--- blog/models.py
+++ blog/models.py
@@ -17,14 +17,14 @@ class Category(models.Model):
 
 
 class Post((models.Model)):
-    name = models.CharField(max_length=50, validators=[
-                            MinLengthValidator(5)])
-    content = models.TextField(max_length=2500, validators=[
-        MinLengthValidator(500)])
+    title = models.CharField(max_length=100, validators=[
+        MinLengthValidator(20)])
+    content = models.TextField(validators=[
+        MinLengthValidator(300)])
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title + '- by ' + self.user.username
+        return self.title + '- by ' + self.author.username
