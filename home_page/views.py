69594d9d23e0ce22ec49190693a83d307a76be59
Juan Vargas <juandeveloper19@gmail.com>
2024-11-24 01:48:12 -0500

    Updated home_page view to display categories from database.
    - Updated the home_page view to fetch categories from the database.
    - Replaced the static content with dynamic content displaying categories.
    - Modified the home_page.html template to loop through and display categories.
    
    This update ensures that the home page dynamically displays categories from the database, enhancing the user experience by providing up-to-date content.

--- home_page/views.py
+++ home_page/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from blog.models import Category
 
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    categories = Category.objects.all()
+    return render(request, 'home_page.html', {
+        'categories': categories})
